47927b7950b6db8aa1adb8eefe5e40959d545d22
kinus00 <usuncodes@gmail.com>
2024-08-29 13:13:33 +0100

    add 13-host-application

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,6 @@ const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-console.log(process.env.NODE_ENV );
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
@@ -15,6 +14,5 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-console.log(process.env.DATABASE_URL);
 
 module.exports = new Pool(config);
