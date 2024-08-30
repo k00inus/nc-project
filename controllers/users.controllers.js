fd34b77be7352a9acb416e22d86d608414a3110b
kinus00 <usuncodes@gmail.com>
2024-08-30 08:33:50 +0100

    add 16-get-api-users-username

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,9 +1,20 @@
-const { fetchAllUsers } = require("../models/users.models");
+const { fetchAllUsers, selectUser } = require("../models/users.models");
 
 exports.getUsers = (req, res, next) => {
   fetchAllUsers()
     .then((users) => {
       return res.status(200).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;  
+  selectUser(username)
+    .then((user) => {      
+      return res.status(200).send({ user });
     })
     .catch((err) => {
       next(err);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,8 @@
-const { getUsers } = require('../controllers/users.controllers')
+const { getUsers, getUserByUsername } = require('../controllers/users.controllers')
 
 const router = require('express').Router()
 
 router.get('/', getUsers)
+router.get('/:username', getUserByUsername)
 
 module.exports = router
